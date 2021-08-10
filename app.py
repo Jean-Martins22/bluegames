b17c273c92058414306e1c8319bcc66ca00a0d8d
caiomanfredini <caiomanfredini.15@gmail.com>
2021-08-10 20:10:27 -0300

    fix/(pages): estruturar folders e add routes

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
 from flask import Flask, render_template, redirect, request, session, flash
-from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://azdzqgug:OfKkSBFGLAzhDvyH1BggTpnBOst1M_po@kesavan.db.elephantsql.com/azdzqgug'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Jogo(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(150), nullable=False)
-    imagem = db.Column(db.String(500), nullable=False)
-    descricao = db.Column(db.String(500), nullable=False)
-    link = db.Column(db.String(300), nullable=False)
+@app.route('/')
+def home():
+    return render_template('landing_page.html')
 
-    def __init__(self, nome, imagem, descricao, link):
-        self.nome = nome
-        self.imagem = imagem
-        self.descricao = descricao
-        self.link = link
+
+@app.route('/dashboard')
+def dash():
+    return render_template('dashboard.html')
+
+# class Jogo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     nome = db.Column(db.String(150), nullable=False)
+#     imagem = db.Column(db.String(500), nullable=False)
+#     descricao = db.Column(db.String(500), nullable=False)
+#     link = db.Column(db.String(300), nullable=False)
+
+#     def __init__(self, nome, imagem, descricao, link):
+#         self.nome = nome
+#         self.imagem = imagem
+#         self.descricao = descricao
+#         self.link = link
 
 
 if __name__ == '__main__':
-    db.create_all()
+    # db.create_all()
     app.run(debug=True)
-    
-    sims = Jogo("The Sims", "sims.png", "Jogo de pessoa", "www.sims.com")
-    db.session.add(sims)
-    db.session.commit()
+
+    # sims = Jogo("The Sims", "sims.png", "Jogo de pessoa", "www.sims.com")
+    # db.session.add(sims)
+    # db.session.commit()
