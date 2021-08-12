b98bd689595c3e6e4a02d3bf809d9b47102e4615
caiomanfredini <caiomanfredini.15@gmail.com>
2021-08-12 09:49:13 -0300

    fix(admin_page): bugs e css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, session, flash
+from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,23 +7,38 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/')
-def home():
-    return render_template('landing_page.html')
-
-
 class Games(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(52), nullable=True)
-    max_players = db.Column(db.Integer, nullable=True)
-    genre = db.Column(db.String(52), nullable=True)
-    publisher = db.Column(db.String(20), nullable=True)
-    price = db.Column(db.Float, nullable=True)
-    release_year = db.Column(db.Integer, nullable=True)
-    # brand_new = db.Column(db.Boolean, nullable=False)
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    title = db.Column(
+        db.String(52),
+        nullable=True
+    )
+    max_players = db.Column(
+        db.Integer,
+        nullable=True
+    )
+    genre = db.Column(
+        db.String(52),
+        nullable=True
+    )
+    publisher = db.Column(
+        db.String(20),
+        nullable=True
+    )
+    price = db.Column(
+        db.Float,
+        nullable=True
+    )
+    release_year = db.Column(
+        db.Integer,
+        nullable=True
+    )
 
-
-    def __init__(self,title, max_players, genre, publisher, price, release_year):
+    def __init__(self, title, max_players, genre, publisher, price, release_year):
         self.title = title
         self.max_players = max_players
         self.genre = genre
@@ -31,53 +46,67 @@ class Games(db.Model):
         self.price = price
         self.release_year = release_year
 
-@app.route('/dashboard')
-def dash():
+
+@app.route('/')
+def home():
+    return render_template('landing_page.html')
+
+
+@app.route('/dash')
+def go_dash():
     games = Games.query.all()
-    return render_template('dashboard.html',games=games)
-    
-@app.route('/gameslist')
-def gamelist():
+    return render_template('dashboard.html', games=games)
+
+
+@app.route('/list')
+def go_list():
     games = Games.query.all()
     return render_template('gameslist.html', games=games)
 
 
 @app.route('/admin')
-def adm():
+def go_adm():
     games = Games.query.all()
     return render_template('admin.html', games=games)
 
-@app.route('/admin/<id>', methods=['GET','POST'])
-def edit(id):
-   gameEdit = Games.query.get(id)
-   if request.method == 'POST':
-      gameEdit.title = request.form['title']
-      gameEdit.genre = request.form['genre']
-      gameEdit.max_players = request.form['max_players']
-      gameEdit.price = request.form['price']
-      gameEdit.publisher = request.form['publisher']
-      gameEdit.release_year = request.form['release_year']
-      db.session.commit()
-      return redirect('/admin')
+@app.route('/add')
+def go_add():
+    return render_template()
 
-   return render_template('admin.html', gameEdit=gameEdit)
 
-@app.route('/add', methods=['GET', 'POST'])
-def add_game():
-   if request.method == 'POST':
-      game = Games(
-        request.form['title'],
-        request.form['max_players'],
-        request.form['genre'],
-        request.form['publisher'],
-        request.form['price'],
-        request.form['release_year'])
-      db.session.add(game)
-      db.session.commit()
-   return redirect('/admin')
+@app.route('/add_game', methods=['GET', 'POST'])
+def adm_add():
+    if request.method == 'POST':
+        game = Games(
+            request.form['title'],
+            request.form['max_players'],
+            request.form['genre'],
+            request.form['publisher'],
+            request.form['price'],
+            request.form['release_year'])
+        db.session.add(game)
+        db.session.commit()
+    return redirect('/admin')
 
-@app.route('/delete/<id>', methods=['GET', 'POST'])
-def delete_game(id):
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def adm_edit(id):
+    gameEdit = Games.query.get(id)
+    if request.method == 'POST':
+        gameEdit.title = request.form['title']
+        gameEdit.genre = request.form['genre']
+        gameEdit.max_players = request.form['max_players']
+        gameEdit.price = request.form['price']
+        gameEdit.publisher = request.form['publisher']
+        gameEdit.release_year = request.form['release_year']
+        db.session.commit()
+        return redirect('/admin')
+
+    return render_template('admin.html', gameEdit=gameEdit)
+
+
+@app.route('/del/<id>', methods=['GET', 'POST'])
+def adm_del(id):
     gameEdit = Games.query.get(id)
     db.session.delete(gameEdit)
     db.session.commit()
@@ -85,9 +114,5 @@ def delete_game(id):
 
 
 if __name__ == '__main__':
-    # db.create_all()
+    db.create_all()
     app.run(debug=True)
-
-    # sims = Jogo("The Sims", "sims.png", "Jogo de pessoa", "www.sims.com")
-    # db.session.add(sims)
-    # db.session.commit()
