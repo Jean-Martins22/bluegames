05669c82ab2d80264b3943533510ff0626eb2ae5
caiomanfredini <caiomanfredini.15@gmail.com>
2021-08-12 16:12:19 -0300

    ultimas mudanças no dash

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nope:TaQhYFew5JO6OxG_X3Vwq-UyP5FzqmTL@kesavan.db.elephantsql.com/nope'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tmdbmgbz:TaQhYFew5JO6OxG_X3Vwq-UyP5FzqmTL@kesavan.db.elephantsql.com/tmdbmgbz'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Games(db.Model):
     id = db.Column(
@@ -67,6 +68,7 @@ def go_list():
 def go_adm():
     games = Games.query.all()
     return render_template('admin.html', games=games)
+
 
 @app.route('/add')
 def go_add():
